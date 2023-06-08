4b35e5f90c259f036588a4b8077b2fc0984267c1
Brayden Anderson <braydenanderson2014@gmail.com>
2023-06-08 11:53:37 -0600

    Expanding Colors for console

--- Alpha_1-0-0/solar/src/main/java/com/solarrentalmac/Logging/MessageProcessor.java
+++ Alpha_1-0-0/solar/src/main/java/com/solarrentalmac/Logging/MessageProcessor.java
@@ -20,13 +20,24 @@ public class MessageProcessor {
         2, "[Debug]: "
     );
     private static List<String> colorCodes = List.of(
-        "\033[0;31m", // RED
-        "\033[0;33m", // YELLOW
-        "\033[0;32m", // GREEN
-        "\033[0;34m", // BLUE
-        "\033[0;36m", // CYAN
-        "\033[0;35m"  // MAGENTA
+        "\u001B[30m", // BLACK
+        "\u001B[31m", // RED
+        "\u001B[32m", // GREEN
+        "\u001B[33m", // YELLOW
+        "\u001B[34m", // BLUE
+        "\u001B[35m", // PURPLE
+        "\u001B[36m", // CYAN
+        "\u001B[37m", // WHITE
+        "\u001B[90m", // BRIGHT_BLACK
+        "\u001B[91m", // BRIGHT_RED
+        "\u001B[92m", // BRIGHT_GREEN
+        "\u001B[93m", // BRIGHT_YELLOW
+        "\u001B[94m", // BRIGHT_BLUE
+        "\u001B[95m", // BRIGHT_PURPLE
+        "\u001B[96m", // BRIGHT_CYAN
+        "\u001B[97m"  // BRIGHT_WHITE
     );
+
     private static Map<Integer, Integer> messageTypeColorIndex = new HashMap<>();
     private static Map<Integer, String> messageColors = new HashMap<>() {{
         put(-2, "\033[0;31m"); // RED
